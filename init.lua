e2245d26b5f8fef5c97abadb4561bf5b6b0388b9
Enoch Xu <enochxu@quadric.io>
2023-12-14 13:27:42 -0800

    silence warnings

--- init.lua
+++ init.lua
@@ -3,4 +3,18 @@ require("enoch.lazy")
 
 local path = vim.fn.stdpath("config") .. "/ginit.vim"
 local source_cmd = "source " .. path
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    -- disable virtual text
+    virtual_text = false,
+
+    -- show signs
+    signs = true,
+
+    -- delay update diagnostics
+    update_in_insert = false,
+  }
+)
+
 vim.cmd(source_cmd)
