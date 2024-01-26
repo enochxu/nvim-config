cf1d03bde0da3c21deaec7f6fb254f4b820ead94
Enoch Xu <enochxu@quadric.io>
2024-01-25 22:30:07 -0800

    add poppup error stuff

--- init.lua
+++ init.lua
@@ -4,6 +4,12 @@ require("enoch.lazy")
 local path = vim.fn.stdpath("config") .. "/ginit.vim"
 local source_cmd = "source " .. path
 
+-- lsp stuff
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     -- disable virtual text
